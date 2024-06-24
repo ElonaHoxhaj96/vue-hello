792a6fb27f384238bd42b77a5a952bf8ca4449fd
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-24 17:27:39 +0200

    feat: extrapolated VUE apps from the framework and created APP

---
+++ main.js
@@ -0,0 +1,10 @@
+// estrapolo dal framework il metodo per creare l'app VUE
+const {createApp} = Vue;
+//Creo l'istanza di un app Vue e la collego al mio tag contenitore in HTML
+createApp({
+    data() {
+        return {
+            myTitle: "Hello World!! Welcome to my website",
+        }
+    },
+})
